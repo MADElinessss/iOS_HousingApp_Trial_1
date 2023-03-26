a47f2f7ca24fd88846bf161222987416d6b3fcf9
MADElinessss <jyseen@naver.com>
2023-03-26 18:48:21 +0900

    Initial Commit

---
+++ HousingApp1/HousingApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  HousingApp1App.swift
+//  HousingApp1
+//
+//  Created by 신정연 on 2023/03/26.
+//
+
+import SwiftUI
+
+@main
+struct HousingApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
